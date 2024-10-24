2d67a5b655b8126736de5b64b4314472d4f71c52
Sebastia n <KamashiVentures@gmail.com>
2024-10-24 15:34:51 +0200

    Test

--- Zadanie 1.2/Program.cs
+++ Zadanie 1.2/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
-class Program() { 
+class Program()
+{
 
-static void Main (){
-    Console.Write("Witaj, jak masz na imię? ");
+    static void Main()
+    {
+        Console.Write("Witaj, jak masz na imię? ");
         string imie = Console.ReadLine();
         Console.Write("Witaj " + imie + "!");
     }
